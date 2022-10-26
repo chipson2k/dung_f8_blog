16013f1e8e700249122ac97ea245b74510c84670
Nguyen Dung <dungvang033@gmail.com>
2022-10-26 22:11:04 +0700

    insert data to DB

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,12 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const slug = require('mongoose-slug-generator');
+
+// Add plugins
+mongoose.plugin(slug);
 
 const Courses = new Schema({
-    name: { type: String, maxLength: 255 },
-    desc: { type: String, maxLength: 600 },
-    image: { type: String, maxLength: 800 },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
+    name: { type: String, require: true },
+    desc: { type: String },
+    image: { type: String },
+    videoId: {type: String, require: true },
+    // unique: slug sẽ thay đổi nếu tồn tại slug cùng tên trong DB
+    slug: { type: String, slug: 'name', unique: true },
+}, {
+    timestamps: true
 });
+
 
 module.exports = mongoose.model('Courses', Courses);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,18 @@
 const newsRouter = require('./news');
 const siteRouter = require('./site');
+const coursesRouter = require('./courses');
 
 function route(app) {
     // mặc định: /news sẽ vào newsRouter
     app.use('/news', newsRouter);
 
+    // mặc định: /courses sẽ vào coursesRouter
+    app.use('/courses', coursesRouter);
+
     // mặc định: / sẽ vào siteRouter
     app.use('/', siteRouter);
+
+    
 }
 
 module.exports = route;
